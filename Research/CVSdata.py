c3c73cc3e418070669e8a119024c1a5bd8081a2c
blairaweaver <blairaweaver@yahoo.com>
2020-03-10 21:05:34 -0400

    Finished code to find local mins and maxs. Unfortunately, due to the current algorithm, it doesn't work well for flat peaks and valleys

--- Research/CVSdata.py
+++ Research/CVSdata.py
@@ -25,9 +25,9 @@ def updateDF(data):
         data.at[index, 'day'] = dt.datetime.date(row['date'])
         data.at[index, 'time'] = dt.datetime.time(row['date'])
         if not index == 0:
-            temp = float(data.at[index, 'close'])
-            temp2 = float(data.at[index - 1, 'close'])
-            data.at[index, 'diff'] = temp2 - temp
+            # temp = float(data.at[index, 'close'])
+            # temp2 = float(data.at[index - 1, 'close'])
+            data.at[index, 'diff'] = data.at[index, 'close'] - data.at[index - 1, 'close']
         else:
             data.at[index, 'diff'] = 0
 
@@ -99,35 +99,43 @@ def setDirection(diff):
 # May.to_csv('AfterImport.csv')
 
 # From this comment to the Comment labeled "STOP" does the segmentation on fakedate file
-May = importFile("fakedata.csv")
+May = importFile("1905hfp.csv")
 May = makeDate(May)
 
 updateDF(May)
 
 May_Day1 = May[May['day'] == dt.date(2019, 5, 2)]
-print(May_Day1.head())
-print(May_Day1['close'].max())
-May_Day1_Max = May_Day1[May_Day1['close']==May_Day1['close'].max()]
-print(May_Day1_Max.head())
+# print(May_Day1.head())
+# print(May_Day1['close'].max())
+# May_Day1_Max = May_Day1[May_Day1['close']==May_Day1['close'].max()]
+# print(May_Day1_Max.head())
 
 May_Day1.plot(x = 'time', y = 'close', kind = 'line')
 
 direction = 0
 numOfPoints = 0
-pointThreshold = 3
-maxStepThreshold = 0.5
-minStepThreshold = 0.1
+pointThreshold = int(input("Point Threshold:"))
+maxStepThreshold = float(input("Max Step Threshold:"))
+minStepThreshold = float(input("Min Step Threshold:"))
+
+# print(type(pointThreshold))
+# print(type(maxStepThreshold))
+
 minPoints =[]
 maxPoints = []
+
+# IMPORTANT:: This does not take into account a diff a zero!!!!!!!
 for index, row in May_Day1.iterrows():
     # if we don't have a trend up or down (ie, just started), run this loop
     if direction == 0:
         # if we haven't increase or decrease from previous, just continue on (ie first element)
         if May_Day1.at[index, 'diff'] == 0:
+            numOfPoints += 1
             continue
         # if we have changed from previous, use this to set the direction (ie second element)
         else:
             direction = setDirection(May_Day1.at[index, 'diff'])
+            numOfPoints += 1
             continue
     # if we are still going in the same direction (decreasing or increasing), add to the number of points and continue to next point
     if checkDirection(direction, May_Day1.at[index, 'diff']):
@@ -136,49 +144,78 @@ for index, row in May_Day1.iterrows():
     # if we don't, then check to see if we meet the qualifications for local min or max
     else:
         # if there is a big enough step to call a local min or max
-        if May_Day1.at[index, 'diff'] > maxStepThreshold:
-            pass
+        if abs(May_Day1.at[index, 'diff']) > maxStepThreshold:
+            if direction == -1:
+                minPoints.append(May_Day1.iloc[index-1 -May_Day1.index[0]])
+                direction = 1
+            else:
+                maxPoints.append(May_Day1.iloc[index-1-May_Day1.index[0]])
+                direction = -1
+            numOfPoints = 2
+            continue
         # if it is big enough to check
-        elif May_Day1.at[index, 'diff'] > minStepThreshold:
+        elif abs(May_Day1.at[index, 'diff']) > minStepThreshold:
             # if we have enough points to classify as min or max
             if numOfPoints > pointThreshold:
-
-                pass
+                if direction == -1:
+                    minPoints.append(May_Day1.iloc[index - 1-May_Day1.index[0]])
+                    direction = 1
+                else:
+                    maxPoints.append(May_Day1.iloc[index - 1-May_Day1.index[0]])
+                    direction = -1
+                numOfPoints = 2
             else:
                 numOfPoints = 2
                 direction = setDirection(May_Day1.at[index, 'diff'])
-                pass
-            pass
-        else:
-            # check to see if it continuews in the same direction after
-            if checkDirection(direction, May_Day1.at[index + 1, 'diff']):
-                # check to see if it continues in the same direction
-                pass
+        # had code for if under the min threshold to check next, but I believe the min threshold should be set so that these can be ignored
+        # ie, points under the min threshold are essentially flat
+        # else:
+        #     if index == May_Day1.last_valid_index():
+        #         break
+        #     # check to see if it continues in the same direction after
+        #     if checkDirection(direction, May_Day1.at[index + 1, 'diff']):
+        #         continue
+        #     else:
+        #         numOfPoints = 2
+        #         setDirection(May_Day1.at[index, 'diff'])
 
 
-Day1Segments = []
-endindex = May_Day1.last_valid_index() - May_Day1.index[0]
-for idx in reversed(May_Day1_Max.index):
-    temp = idx - May_Day1.index[0]
-    # print(endindex)
-    # print(idx)
-    # print(May_Day1_Max.at[idx, 'time'])
-    print("{0}, {1}".format(temp, endindex))
-    print(May_Day1.iloc[temp:endindex, :])
-    print(May_Day1.iloc[0:200, :])
-    # print(May_Day1.iloc[idx:endindex, :])
-    Day1Segments.append(May_Day1.iloc[temp:endindex, :])
-    endindex = temp - 1
-print(endindex)
-print(May_Day1.index[0])
-print(May_Day1.index)
-if endindex > 0:
-    print(May_Day1.iloc[0:endindex, :])
-    Day1Segments.append(May_Day1.iloc[0:endindex, :])
 
-print(Day1Segments[-1].head())
-print(May_Day1['date'].iloc[-1])
-print(May_Day1.last_valid_index())
+for i in minPoints:
+    # print(i['close'])
+    plt.plot_date(i['time'], i['close'])
+
+for i in maxPoints:
+    plt.plot_date(i['time'], i['close'])
+    # print(i)
+
+plt.savefig('testplotMinMax{:02d}_{:0.2f}_{:0.2f}.png'.format(pointThreshold, minStepThreshold, maxStepThreshold))
+plt.show()
+
+#
+# Day1Segments = []
+# endindex = May_Day1.last_valid_index() - May_Day1.index[0]
+# for idx in reversed(May_Day1_Max.index):
+#     temp = idx - May_Day1.index[0]
+#     # print(endindex)
+#     # print(idx)
+#     # print(May_Day1_Max.at[idx, 'time'])
+#     print("{0}, {1}".format(temp, endindex))
+#     print(May_Day1.iloc[temp:endindex, :])
+#     print(May_Day1.iloc[0:200, :])
+#     # print(May_Day1.iloc[idx:endindex, :])
+#     Day1Segments.append(May_Day1.iloc[temp:endindex, :])
+#     endindex = temp - 1
+# print(endindex)
+# print(May_Day1.index[0])
+# print(May_Day1.index)
+# if endindex > 0:
+#     print(May_Day1.iloc[0:endindex, :])
+#     Day1Segments.append(May_Day1.iloc[0:endindex, :])
+#
+# print(Day1Segments[-1].head())
+# print(May_Day1['date'].iloc[-1])
+# print(May_Day1.last_valid_index())
 
 # "STOP"
 
